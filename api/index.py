8027665d039099ea6b796f015d15e0ed5c21c0c9
Graham Tackley <graham@tackley.net>
2024-03-04 20:48:57 +0000

    testing of query parameters

--- api/index.py
+++ api/index.py
@@ -1,8 +1,14 @@
 from .data import getData
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
   return getData("Captain of Industry", parameter="machines")
+
+@app.route('/items', methods=["GET"])
+def homepage():
+  queryParameters = request.args
+  return getData(queryParameters.get("game"), parameter="items")

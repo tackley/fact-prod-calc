091c96a550b28ad0ab71a422e2396dd2e5acc230
Graham Tackley <graham@tackley.net>
2024-03-05 09:06:21 +0000

    try adding in a next website too!

--- api/index.py
+++ api/index.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 games = {
   "coi":"Captain of Industry",
 }
-@app.route('/')
+@app.route('/api')
 def homepage():
   return "Welcome to the Factory Production Calculator v2.0!"
 
-@app.route('/<game>/items')
+@app.route('/api/<game>/items')
 def itemList(game):
   return getData(games[game], parameter="items")
 
-@app.route('/<game>/recipes', methods=["GET"])
+@app.route('/api/<game>/recipes', methods=["GET"])
 def recipes(game):
   queryParameters = request.args
   item = queryParameters.get("item")

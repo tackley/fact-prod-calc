48ef1fc7412ab31e77f31a0ea23c48e068b04a85
Graham Tackley <graham@tackley.net>
2024-03-06 07:51:15 +0000

    basic mui integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,27 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Inter, Roboto } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "../src/theme";
+import { CssBaseline } from "@mui/material";
 
 export const metadata = {
-  title: 'Fact! Prod! Calc!',
-  description: 'Factories. Production. Calculations.',
-}
+  title: "Fact! Prod! Calc!",
+  description: "Factories. Production. Calculations.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+// src/theme.ts
+"use client";
+import { Roboto } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+

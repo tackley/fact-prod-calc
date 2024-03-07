b15493decdf3f373b01f0fc4132b426d243c9513
Graham Tackley <graham@tackley.net>
2024-03-07 21:29:06 +0000

    fix build error!

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import { Box } from "@mui/material";
 import { ItemSelect } from "./_components/ItemSelect";
 import { useState } from "react";
-import { fetchItems } from "./_backend/backend";
 
 export default function Home() {
   const [item, setItem] = useState<string>();
